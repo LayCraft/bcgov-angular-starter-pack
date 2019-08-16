242261188eca5f71071cbfb5c3ef04b62ebd2b75
Curtis <curtis.laycraft@zoho.com>
2019-08-16 10:17:04 -0700

    incorporate font awesome dependency

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path:'anonymous',
-    loadChildren: () => import('./anonymous/anonymous.module').then(mod=>mod.AnonymousModule)
+    path: 'anonymous',
+    loadChildren: () => import('./anonymous/anonymous.module').then(mod => mod.AnonymousModule)
   },
   {
-    path:'authenticated',
-    loadChildren: () => import('./authenticated/authenticated.module').then(mod=>mod.AuthenticatedModule)
+    path: 'authenticated',
+    loadChildren: () => import('./authenticated/authenticated.module').then(mod => mod.AuthenticatedModule)
   },
   {
-    path:'',
-    // redirectTo: 'home',
+    path: '',
     component: LandingPageComponent,
     pathMatch: 'full'
   },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  }
 ];
 
 @NgModule({

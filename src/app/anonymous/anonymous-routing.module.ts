d41338aab180b614ecc2974108bd52a782938664
Curtis <curtis.laycraft@zoho.com>
2019-08-16 15:35:13 -0700

    set page structure

--- src/app/anonymous/anonymous-routing.module.ts
+++ src/app/anonymous/anonymous-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AnonymousFormComponent } from './anonymous-form/anonymous-form.component';
+import { AnonymousComponent } from './anonymous.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component: AnonymousFormComponent
+    path: '',
+    component: AnonymousComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'anonymous-form',
+      },
+      {
+        path: 'anonymous-form',
+        component: AnonymousFormComponent
+      }
+    ]
   }
 ];
 
--- src/app/anonymous/anonymous.module.ts
+++ src/app/anonymous/anonymous.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnonymousRoutingModule } from './anonymous-routing.module';
 import { AnonymousFormComponent } from './anonymous-form/anonymous-form.component';
+import { AnonymousComponent } from '../anonymous/anonymous.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AnonymousRoutingModule
   ],
-  declarations: [AnonymousFormComponent]
+  declarations: [AnonymousFormComponent, AnonymousComponent]
 })
 export class AnonymousModule { }

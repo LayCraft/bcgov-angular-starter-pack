2badcffbb4f2c22d175e09d4c6e9efa6853c0f22
Curtis <curtis.laycraft@zoho.com>
2019-08-16 15:03:29 -0700

    set routing into child component

--- src/app/authenticated/authenticated-routing.module.ts
+++ src/app/authenticated/authenticated-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SecretPageComponent } from './secret-page/secret-page.component';
+import { SecretFormComponent } from './secret-form/secret-form.component';
+import { AuthenticatedComponent } from './authenticated.component';
 
 
 const routes: Routes = [
   {
-    path:'', component: SecretPageComponent
+    path: '',
+    component: AuthenticatedComponent,
+    children: [
+      {
+        // set the default component to route to for this user
+        path: '',
+        redirectTo: 'secret-page',
+        pathMatch: 'full'
+      },
+      {
+        path: 'secret-page',
+        component: SecretPageComponent
+      },
+      {
+        path: 'secret-form',
+        component: SecretFormComponent
+      }
+    ]
   },
 ];
 
--- src/app/authenticated/authenticated.module.ts
+++ src/app/authenticated/authenticated.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticatedRoutingModule } from './authenticated-routing.module';
 import { SecretPageComponent } from './secret-page/secret-page.component';
-
-
+import { AuthenticatedComponent } from '../authenticated/authenticated.component';
+import { SecretFormComponent } from './secret-form/secret-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthenticatedRoutingModule
   ],
-  declarations: [SecretPageComponent]
+  declarations: [
+    SecretPageComponent,
+    AuthenticatedComponent,
+    SecretFormComponent
+  ]
 })
 export class AuthenticatedModule { }

f816fc24cf6feaf6639a5369c987ab1b54a99432
Curtis <curtis.laycraft@zoho.com>
2019-08-29 11:05:13 -0700

    create material component for demo

--- src/app/anonymous/anonymous.module.ts
+++ src/app/anonymous/anonymous.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { AnonymousRoutingModule } from './anonymous-routing.module';
 import { AnonymousFormComponent } from './anonymous-form/anonymous-form.component';
 import { AnonymousComponent } from '../anonymous/anonymous.component';
+import { MatDemoComponent } from './mat-demo/mat-demo.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AnonymousRoutingModule
   ],
-  declarations: [AnonymousFormComponent, AnonymousComponent]
+  declarations: [AnonymousFormComponent, AnonymousComponent, MatDemoComponent]
 })
 export class AnonymousModule { }
